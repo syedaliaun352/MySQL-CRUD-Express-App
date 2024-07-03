f9624146afa05e534c53e85d710d3f590d137460
Syed Ali Aun <aas745@hotmail.com>
2024-07-03 22:43:04 +0500

    add env and update db.js

--- src/index.mjs
+++ src/index.mjs
@@ -1,39 +1,63 @@
 import express from 'express';
-import mysql from 'mysql2';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import pool from './db.mjs';
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4300;
-app.use(cors());
-const db = mysql.createConnection({
-    host: '51.112.70.161',
-    user: 'api_user',
-    password: "api1992",
-    database: "api_db"
-});
+const port = process.env.PORT || 3000;
 
-db.connect(err => {
-    if (err) {
-        console.error("Error connecting to database:", err);
-    } else {
-        console.log("Connected to database");
-    }
-});
+app.use(cors());
+app.use(express.json());
+
+(async () => {
+  try {
+    await pool.getConnection();
+    console.log("Connected to the database");
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+    process.exit(1)
+  }
+})();
 
 app.get("/", (req, res) => {
-    res.send("API is Running");
+  res.send("API is Running");
 });
 
-app.get("/api/users", (req, res) => {
+app.get("/api/users", async (req, res) => {
+  try {
     const sql = 'SELECT * FROM users';
-    db.query(sql, (err, results) => {
-        if (err) {
-            return res.sendStatus(500).send(err);
-        }
-        res.send(results);
-    });
+    const [results] = await pool.query(sql);
+    res.json(results);
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/api/users/:id', async (req, res) => {
+  const userid = parseInt(req.params.id);
+
+  if (isNaN(userId)) {
+    return res.sendStatus(400);
+  }
+
+  const sql = 'SELECT * FROM users WHERE id = ?';
+  try {
+    const [results] = await pool.query(sql, [userid]);
+
+    if (results.length === 0) {
+      return res.sendStatus(404);
+    }
+
+    res.json(results[0]);
+  } catch (err) {
+    console.error("Error fetching user by ID:", err);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
+  console.log(`Listening on port: ${port}`);
 });
