8c049817d0fe1b918e3556e76ff1c1868af7e589
Syed Ali Aun <aas745@hotmail.com>
2024-07-17 04:35:23 +0500

    Update userRoutes.mjs

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -1,13 +1,14 @@
 import express from 'express';
 import { registerUser, getUsers, getUserById, deleteUser, updateUser } from '../controllers/userController.mjs';
 import { validateRegisterInput } from '../validations/userValidation.mjs';
+import limiter from '../middlewares/ratelimit.mjs';
 
 const router = express.Router();
 
-router.post('/register', validateRegisterInput, registerUser);
-router.get('/users', getUsers);
-router.get('/users/:id', getUserById);
-router.delete('/users/:id', deleteUser);
-router.put('/users/:id', updateUser);
+router.post('/register',limiter, validateRegisterInput, registerUser);
+router.get('/users',limiter, getUsers);
+router.get('/users/:id',limiter, getUserById);
+router.delete('/users/:id',limiter, deleteUser);
+router.put('/users/:id', limiter, updateUser);
 
 export default router;
