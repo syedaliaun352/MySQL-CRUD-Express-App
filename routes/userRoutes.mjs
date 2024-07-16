8370a49280b11587679fca9e5e54b3329396286f
Syed Ali Aun <aas745@hotmail.com>
2024-07-17 04:29:44 +0500

    add user validation

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
 import { registerUser, getUsers, getUserById, deleteUser, updateUser } from '../controllers/userController.mjs';
+import { validateRegisterInput } from '../validations/userValidation.mjs';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', validateRegisterInput, registerUser);
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.delete('/users/:id', deleteUser);
